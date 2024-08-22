01b4918b302338d8eb21a7072596d5e1696bbbd9
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2024-08-22 23:23:31 +0530

    Refactor type parsing

--- src/parser/parse_declaration.rs
+++ src/parser/parse_declaration.rs
@@ -1,6 +1,7 @@
 use crate::{
     helpers::{compiler_error, unexpected_token},
     lexer::{
+        lexer::Token,
         tokens::{Bracket, Number, Operations},
         types::VarType,
     },
@@ -37,6 +38,82 @@ impl Parser {
         }
     }
 
+    pub fn parse_var_type(&mut self) -> (Token, VarType) {
+        let token = self.peek_next_token();
+
+        match &token.token {
+            TokenEnum::Type(var_type) => {
+                let mut actual_var_type = var_type.clone();
+
+                let type_token = self.get_next_token();
+
+                self.check_if_array_type(&mut actual_var_type, var_type);
+
+                return (type_token, actual_var_type);
+            }
+
+            // This could be a user defined type
+            TokenEnum::Variable(var_type_name) => {
+                let var_name_clone = var_type_name.clone();
+
+                let found = self.user_defined_types.iter().find(|var| var.name == *var_name_clone);
+
+                let var_type = if let Some(t) = found {
+                    t.type_.clone()
+                } else {
+                    // FIXME: This shouldn't be here but in semantic analysis phase
+                    compiler_error(format!("No such type '{}'", var_type_name), &token);
+                    exit(1);
+                };
+
+                let type_token = self.get_next_token();
+
+                let mut actual_var_type = var_type.clone();
+
+                self.check_if_array_type(&mut actual_var_type, &var_type);
+
+                return (type_token, actual_var_type);
+            }
+
+            TokenEnum::Op(Operations::Multiply) => {
+                // pointer to a user defined type
+                // Since we can't really lex these
+
+                // consume '*'
+                self.get_next_token();
+
+                let next_token = self.get_next_token();
+
+                if let TokenEnum::Variable(var_type_name) = &next_token.token {
+                    let var_name_clone = var_type_name.clone();
+
+                    let found = self.user_defined_types.iter().find(|var| var.name == *var_name_clone);
+
+                    if found.is_none() {
+                        compiler_error(format!("No such type '{}'", var_type_name), &token);
+                        exit(1);
+                    }
+
+                    let var_type = found.unwrap().type_.clone();
+
+                    let mut actual_var_type = var_type.clone();
+
+                    self.check_if_array_type(&mut actual_var_type, &var_type);
+
+                    return (next_token, VarType::Ptr(Box::new(actual_var_type)));
+                } else {
+                    unexpected_token(&next_token, None);
+                    exit(1);
+                }
+            }
+
+            tok => {
+                unexpected_token(&token, None);
+                exit(1);
+            }
+        }
+    }
+
     /// VARIABLE_DECLARATION -> def VAR_NAME: (*)* VAR_TYPE
     pub fn parse_variable(&mut self) -> Variable {
         let token = self.get_next_token();
@@ -48,101 +125,9 @@ impl Parser {
                 match token.token {
                     // : after variable name, so can only be VAR_NAME: VAR_TYPE
                     TokenEnum::Colon => {
-                        let token = self.peek_next_token();
+                        let (token, var_type) = self.parse_var_type();
 
-                        match &token.token {
-                            TokenEnum::Type(var_type) => {
-                                let mut actual_var_type = var_type.clone();
-
-                                let type_token = self.get_next_token();
-
-                                self.check_if_array_type(&mut actual_var_type, var_type);
-
-                                return Variable::new(
-                                    Box::new(type_token),
-                                    actual_var_type.clone(),
-                                    var_name,
-                                    false,
-                                    false,
-                                    0,
-                                );
-                            }
-
-                            // This could be a user defined type
-                            TokenEnum::Variable(var_type_name) => {
-                                println!("TokenEnum::Variable(var_type_name): {var_type_name}" );
-
-                                let var_name_clone = var_type_name.clone();
-
-                                let found = self.user_defined_types.iter().find(|var| var.name == *var_name_clone);
-
-                                let var_type = if let Some(t) = found {
-                                    t.type_.clone()
-                                } else {
-                                    // This will be fixed in the semantic analysis
-                                    VarType::Unknown
-                                };
-
-                                let type_token = self.get_next_token();
-
-                                let mut actual_var_type = var_type.clone();
-
-                                self.check_if_array_type(&mut actual_var_type, &var_type);
-
-                                return Variable::new(
-                                    Box::new(type_token),
-                                    actual_var_type.clone(),
-                                    var_name.into(),
-                                    false,
-                                    false,
-                                    0,
-                                );
-                            }
-
-                            TokenEnum::Op(Operations::Multiply) => {
-                                // pointer to a user defined type
-                                // Since we can't really lex these
-
-                                // consume '*'
-                                self.get_next_token();
-
-                                let next_token = self.get_next_token();
-
-                                if let TokenEnum::Variable(var_type_name) = &next_token.token {
-                                    let var_name_clone = var_type_name.clone();
-
-                                    let found = self.user_defined_types.iter().find(|var| var.name == *var_name_clone);
-
-                                    if found.is_none() {
-                                        compiler_error(format!("No such type '{}'", var_type_name), &token);
-                                        exit(1);
-                                    }
-
-                                    let var_type = found.unwrap().type_.clone();
-
-                                    let mut actual_var_type = var_type.clone();
-
-                                    self.check_if_array_type(&mut actual_var_type, &var_type);
-
-                                    return Variable::new(
-                                        Box::new(next_token),
-                                        VarType::Ptr(Box::new(actual_var_type)),
-                                        var_name.into(),
-                                        false,
-                                        false,
-                                        0,
-                                    );
-                                } else {
-                                    unexpected_token(&next_token, None);
-                                    exit(1);
-                                }
-                            }
-
-                            tok => {
-                                unexpected_token(&token, None);
-                                exit(1);
-                            }
-                        }
+                        return Variable::new(Box::new(token), var_type, var_name.into(), false, false, 0);
                     }
 
                     _ => {
