fbf2f7cc6181c61a91dd411e584020f09b74c1fe
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2025-01-12 20:21:04 +0530

    Fix test

--- generated/output.asm
+++ generated/output.asm
@@ -4,13 +4,27 @@ section .bss
 	digitSpace resb 100
 	digitSpacePos resb 8
 	argc resb 8
-	lower_char_occurances resb 208
 
 section .data
 	;; For floating point operations
 	float_imm dq 0
-	string_0 db 108,111,119,101,114,95,99,104,97,114,95,111,99,99,117,114,97,110,99,101,115,32,61,32
-	string_1 db 97,100,100,114,95,116,111,95,117,112,100,97,116,101,32,61,32
+	string_0 db 72,101,108,108,111,32,87,111,114,108,100,10
+	string_1 db 120,32,61,32
+	string_2 db 121,32,61,32
+	string_3 db 122,32,61,32
+	string_4 db 119,32,61,32
+	string_5 db 104,101,108,108,111,32,61,32
+	string_6 db 71,111,111,100,98,121,101,32,87,111,114,108,100,10
+	string_7 db 120,32,61,32
+	string_8 db 121,32,61,32
+	string_9 db 122,32,61,32
+	string_10 db 119,32,61,32
+	string_11 db 104,101,108,108,111,32,61,32
+	string_12 db 120,32,61,32
+	string_13 db 121,32,61,32
+	string_14 db 122,32,61,32
+	string_15 db 119,32,61,32
+	string_16 db 104,101,108,108,111,32,61,32
 
 section .text
 	global _start
@@ -25,64 +39,276 @@ _main:
 	push rbp
 	mov rbp, rsp
 	;; Make sure that the stack pointer is 16 byte aligned always
-	sub rsp, 16
+	sub rsp, 32
 	
 	
+	
+	
+	
+	
+	;; Assigning local struct MyStruct
+	;; Member name: x Struct offset = 32. Member offset: 0
+	;; assign_local_number of type Integer
+	mov QWORD [rbp - 32], 400000000
+	
+	;; Member name: y Struct offset = 32. Member offset: 8
+	;; assign_local_number of type Integer32
+	mov DWORD [rbp - 24], 2000000
+	
+	;; Member name: z Struct offset = 32. Member offset: 12
+	;; assign_local_number of type Integer16
+	mov WORD [rbp - 20], 42069
+	
+	;; Member name: w Struct offset = 32. Member offset: 14
 	;; assign_local_number of type Integer8
-	mov BYTE [rbp - 1], 3
+	mov BYTE [rbp - 18], 200
 	
-	;; Global new thingy
-	xor rax, rax
-	mov rax, lower_char_occurances
-	
-	
-	mov rbx, rax ;; Saving rax
-	;; Multiply get the two operands from the stack. Type: Integer
-	xor rdx, rdx
-	mov rax, 0 ;; clearing al
-	mov rcx, 0 ;; clearing cl
-	mov al, 8
-	mov cl, [rbp - 1]
-	mul rcx
-	
-	;; Plus get the two operands from the stack. Result type: Pointer -> Integer. Token: < 125 ./tests/first.cy:5:51 Op(Plus) >
-	mov rcx, rax
-	mov rdx, rbx
-	add rcx, rdx
-	;; will lock rcx. first = rax. second = rbx. Locked: [rbx, rax, rcx, rdx]
-	
-	;; src/asm/variable_assignment.rs:236 assign_local_pointer var 'addr_to_update' of type Integer. times_dereferenced: 0
-	mov rax, rcx
-	mov [rbp - 16], rax
-	
-	
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, string_0
-	mov rdx, 24
-	syscall
-	
-	;; Global new thingy
-	xor rax, rax
-	mov rax, lower_char_occurances
-	
-	call _printRAX
-	
+	mov QWORD [rbp - 8], 12
+	mov QWORD [rbp - 16], string_0
 	
 	
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, string_1
-	mov rdx, 17
+	mov rdx, 4
 	syscall
 	
-	mov rax, [rbp - 16]
-	
+	;; get_vec_for_write_number. stack_member: [rbp - 32], type: Integer
+	;; no xor here
+	mov rax, [rbp - 32]
 	call _printRAX
 	
 	
-	;; 'addr_to_update' at '[rbp - 16]'
-	;; 'inc' at '[rbp - 1]'
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string_2
+	mov rdx, 4
+	syscall
+	
+	;; get_vec_for_write_number. stack_member: [rbp - 24], type: Integer32
+	xor rax, rax
+	mov eax, [rbp - 24]
+	call _printRAX
+	
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string_3
+	mov rdx, 4
+	syscall
+	
+	;; get_vec_for_write_number. stack_member: [rbp - 20], type: Integer16
+	xor rax, rax
+	mov ax, [rbp - 20]
+	call _printRAX
+	
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string_4
+	mov rdx, 4
+	syscall
+	
+	;; get_vec_for_write_number. stack_member: [rbp - 18], type: Integer8
+	xor rax, rax
+	mov al, [rbp - 18]
+	call _printRAX
+	
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string_5
+	mov rdx, 8
+	syscall
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, [rbp - 16]
+	mov rdx, [rbp - 8]
+	syscall
+	
+	
+	
+	;; Plus get the two operands from the stack. Result type: Integer. Token: < 353 ./examples/struct_assign.cy:18:12 Op(Plus) >
+	mov rax, 600
+	mov rbx, [rbp - 32]
+	add rax, rbx
+	;; will lock rax. first = 600. second = [rbp - 32]. Locked: [rax, rbx]
+	
+	;; assign_local_number of type Integer
+	mov QWORD [rbp - 32], rax
+	
+	
+	;; Plus get the two operands from the stack. Result type: Integer32. Token: < 373 ./examples/struct_assign.cy:19:12 Op(Plus) >
+	mov rax, 0 ;; clearing eax
+	mov rbx, 0 ;; clearing ebx
+	mov eax, 400
+	mov ebx, [rbp - 24]
+	add eax, ebx
+	;; will lock rax. first = 400. second = [rbp - 24]. Locked: [rax, rbx]
+	
+	;; assign_local_number of type Integer32
+	mov DWORD [rbp - 24], eax
+	
+	
+	;; Plus get the two operands from the stack. Result type: Integer8. Token: < 393 ./examples/struct_assign.cy:20:12 Op(Plus) >
+	mov rax, 0 ;; clearing al
+	mov rbx, 0 ;; clearing bl
+	mov al, 20
+	mov bl, [rbp - 18]
+	add al, bl
+	;; will lock rax. first = 20. second = [rbp - 18]. Locked: [rax, rbx]
+	
+	;; assign_local_number of type Integer8
+	mov BYTE [rbp - 18], al
+	
+	
+	mov QWORD [rbp - 8], 14
+	mov QWORD [rbp - 16], string_6
+	
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string_7
+	mov rdx, 4
+	syscall
+	
+	;; get_vec_for_write_number. stack_member: [rbp - 32], type: Integer
+	;; no xor here
+	mov rax, [rbp - 32]
+	call _printRAX
+	
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string_8
+	mov rdx, 4
+	syscall
+	
+	;; get_vec_for_write_number. stack_member: [rbp - 24], type: Integer32
+	xor rax, rax
+	mov eax, [rbp - 24]
+	call _printRAX
+	
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string_9
+	mov rdx, 4
+	syscall
+	
+	;; get_vec_for_write_number. stack_member: [rbp - 20], type: Integer16
+	xor rax, rax
+	mov ax, [rbp - 20]
+	call _printRAX
+	
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string_10
+	mov rdx, 4
+	syscall
+	
+	;; get_vec_for_write_number. stack_member: [rbp - 18], type: Integer8
+	xor rax, rax
+	mov al, [rbp - 18]
+	call _printRAX
+	
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string_11
+	mov rdx, 8
+	syscall
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, [rbp - 16]
+	mov rdx, [rbp - 8]
+	syscall
+	
+	
+	
+	mov rax, [rbp - 32]
+	mov rbx, 600
+	add rax, rbx
+	mov [rbp - 32], rax
+	
+	
+	mov rax, [rbp - 24]
+	mov rbx, 400
+	add rax, rbx
+	mov [rbp - 24], rax
+	
+	
+	mov rax, [rbp - 18]
+	mov rbx, 20
+	add rax, rbx
+	mov [rbp - 18], rax
+	
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string_12
+	mov rdx, 4
+	syscall
+	
+	;; get_vec_for_write_number. stack_member: [rbp - 32], type: Integer
+	;; no xor here
+	mov rax, [rbp - 32]
+	call _printRAX
+	
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string_13
+	mov rdx, 4
+	syscall
+	
+	;; get_vec_for_write_number. stack_member: [rbp - 24], type: Integer32
+	xor rax, rax
+	mov eax, [rbp - 24]
+	call _printRAX
+	
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string_14
+	mov rdx, 4
+	syscall
+	
+	;; get_vec_for_write_number. stack_member: [rbp - 20], type: Integer16
+	xor rax, rax
+	mov ax, [rbp - 20]
+	call _printRAX
+	
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string_15
+	mov rdx, 4
+	syscall
+	
+	;; get_vec_for_write_number. stack_member: [rbp - 18], type: Integer8
+	xor rax, rax
+	mov al, [rbp - 18]
+	call _printRAX
+	
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string_16
+	mov rdx, 8
+	syscall
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, [rbp - 16]
+	mov rdx, [rbp - 8]
+	syscall
+	
+	
+	;; 'a' at '[rbp - 32]'
 	mov rsp, rbp
 	pop rbp
 	ret
